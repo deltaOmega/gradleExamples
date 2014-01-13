601d7af0385a603a01e08bd122c98fe786733c95
Swati Satpute <swatimhankare@Swati-Satputes-MacBook-Pro-2.local>
2014-01-12 23:56:09 -0500

    adding build.gradle

---
+++ aCommon/src/main/java/CollectionsUtil.java
@@ -0,0 +1,12 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: swatimhankare
+ * Date: 1/12/14
+ * Time: 11:02 PM
+ */
+public class CollectionsUtil {
+
+  public static void sayHello() {
+    System.out.println("hello");
+  }
+}
