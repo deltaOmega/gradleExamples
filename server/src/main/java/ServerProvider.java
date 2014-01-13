7a4f5a2d32475d7fe108addf52e2fc3b541fb21b
Swati Satpute <swatimhankare@Swati-Satputes-MacBook-Pro-2.local>
2014-01-12 23:56:59 -0500

    adding build.gradle

---
+++ server/src/main/java/ServerProvider.java
@@ -0,0 +1,11 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: swatimhankare
+ * Date: 1/12/14
+ * Time: 3:42 PM
+ */
+public class ServerProvider {
+  public static void main(String[] args) {
+    CollectionsUtil.sayHello();
+  }
+}
