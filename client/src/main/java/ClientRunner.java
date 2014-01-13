25051bece072fd85760592dcdf269d6c8342b3f9
Swati Satpute <swatimhankare@Swati-Satputes-MacBook-Pro-2.local>
2014-01-12 23:56:44 -0500

    adding build.gradle

---
+++ client/src/main/java/ClientRunner.java
@@ -0,0 +1,11 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: swatimhankare
+ * Date: 1/12/14
+ * Time: 3:41 PM
+ */
+public class ClientRunner {
+  public static void main(String[] args) {
+  CollectionsUtil.sayHello();
+  }
+}
